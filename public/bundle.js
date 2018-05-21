522dce0f14241bd307f611a57c9c394708f39ed8
Farshid Mahmoodi <farshidmahmoodi2017@gmail.com>
2018-05-21 13:32:25 +0430

    about changed

--- public/bundle.js
+++ public/bundle.js
@@ -26635,9 +26635,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to about page'
+	    )
 	  );
 	};
 
