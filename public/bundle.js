2d7fc2216558393179566f5c10652b8485ac8e73
Farshid Mahmoodi <farshidmahmoodi2017@gmail.com>
2018-05-21 14:39:44 +0430

    add grid

--- public/bundle.js
+++ public/bundle.js
@@ -24918,11 +24918,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      'Main component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row ' },
+	      React.createElement(
+	        'div',
+	        { ClassName: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26769,19 +26772,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
 	    ),
 	    React.createElement(
 	      'p',
 	      null,
-	      'Welcome to examples page'
+	      'Here are a few example locations to try out'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Tehran' },
+	          'Tehran, Iran'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?location=Isfahan' },
+	          'Isfahan, Iran'
+	        )
+	      )
 	    )
 	  );
 	};
