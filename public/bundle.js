63f3c6a7838c53acc604af2a258747e4e59d5978
Farshid Mahmoodi <farshidmahmoodi2017@gmail.com>
2018-05-21 14:55:06 +0430

    new

--- public/bundle.js
+++ public/bundle.js
@@ -25074,7 +25074,7 @@
 	      if (isLoading) {
 	        return React.createElement(
 	          'h3',
-	          null,
+	          { className: 'text-center' },
 	          'Fetching weather...'
 	        );
 	      } else if (temp && location) {
@@ -25086,9 +25086,9 @@
 	      'div',
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
-	        'Weather Component'
+	        'h1',
+	        { className: 'text-center' },
+	        'Get Weather'
 	      ),
 	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
 	      renderMessage()
@@ -25129,7 +25129,7 @@
 	        React.createElement('input', { type: 'text', ref: 'location' }),
 	        React.createElement(
 	          'button',
-	          null,
+	          { className: 'button expanded hollow' },
 	          'Get Weather'
 	        )
 	      )
@@ -25143,7 +25143,7 @@
 /* 227 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
@@ -25152,11 +25152,11 @@
 	      location = _ref.location;
 
 	  return React.createElement(
-	    'h3',
-	    null,
-	    'It\'s ',
+	    "h3",
+	    { className: "text-center" },
+	    "It's ",
 	    temp,
-	    ' in ',
+	    " in ",
 	    location
 	  );
 	};
